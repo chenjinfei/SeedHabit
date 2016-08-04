668688040a84e8f12f3c73f13b2ef9b22f100137
yanqiongnan <578207661@qq.com>
2016-08-04 09:06:30 +0800

    个人资料

---
+++ mySeedHabit/mySeedHabit/General/UserCenter/ViewControllers/EditPersonalTableViewCell.m
@@ -0,0 +1,14 @@
+//
+//  EditPersonalTableViewCell.m
+//  mySeedHabit
+//
+//  Created by lanou on 16/8/3.
+//  Copyright © 2016年 Jinfei Chen. All rights reserved.
+//
+
+#import "EditPersonalTableViewCell.h"
+
+@implementation EditPersonalTableViewCell
+
+
+@end
---
+++ mySeedHabit/mySeedHabit/General/UserCenter/ViewControllers/MyConcernTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MyConcernTableViewController.h
+//  mySeedHabit
+//
+//  Created by lanou on 16/8/3.
+//  Copyright © 2016年 Jinfei Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyConcernTableViewController : UITableViewController
+
+@property (nonatomic,strong)NSString *titleStr;
+
+@end
