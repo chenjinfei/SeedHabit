1a43b6e937afd9ff20e291ae215308971956eed2
yanqiongnan <578207661@qq.com>
2016-08-10 10:53:43 +0800

    add

---
+++ mySeedHabit/mySeedHabit/General/Habit/Views/HabitCheckInCell.m
@@ -0,0 +1,23 @@
+//
+//  HabitCheckInCell.m
+//  mySeedHabit
+//
+//  Created by lanou on 16/8/8.
+//  Copyright © 2016年 Jinfei Chen. All rights reserved.
+//
+
+#import "HabitCheckInCell.h"
+
+@implementation HabitCheckInCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
